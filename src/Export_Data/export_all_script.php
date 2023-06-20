ffbba615f227b527c76da4b90f697f1ce029aaca
Jitca Diana <jitcadiana6@gmail.com>
2023-06-20 14:45:15 +0300

    fixed export_all.php

--- src/Export_Data/export_all_script.php
+++ src/Export_Data/export_all_script.php
@@ -2,7 +2,6 @@
 session_start(); //start the session
 $username = $_SESSION['username'];
 $user_id = $_SESSION['id'];
-$persons = $_POST['persons'];
 $sorted = $_POST['sorted'];
 $format = $_POST['format'];
 
