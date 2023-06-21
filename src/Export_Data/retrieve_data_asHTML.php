b56622309e0b230996bd5e0b21cccfb9288443f1
Jitca Diana <jitcadiana6@gmail.com>
2023-06-21 13:02:13 +0300

    the retrieve_data_asHTML.php is DONE

--- src/Export_Data/retrieve_data_asHTML.php
+++ src/Export_Data/retrieve_data_asHTML.php
@@ -42,145 +42,172 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     //print_r($username);
 
     $conn = DbConnection::getInstance()->getConnection();
-?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>GFG User Details</title>
-        <!-- CSS FOR STYLING THE PAGE -->
-        <style>
-            table {
-                margin: 0 auto;
-                font-size: large;
-                border: 1px solid black;
-            }
+    //select all the data of a user
+    $sql = "SELECT user_id,username,fname,lname,email,secondary_email,function FROM users where username = ?";
+    $stmt = $conn->prepare($sql);
 
-            h1 {
-                text-align: center;
-                color: #006600;
-                font-size: xx-large;
-                font-family: 'Gill Sans', 'Gill Sans MT',
-                ' Calibri', 'Trebuchet MS', 'sans-serif';
-            }
+    $stmt->bind_param("s", $username);
+    $export_data = array();
 
-            td {
-                background-color: #E4F5D4;
-                border: 1px solid black;
-            }
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
 
-            th,
-            td {
-                font-weight: bold;
-                border: 1px solid black;
-                padding: 10px;
-                text-align: center;
-            }
+    $export_data = array();
+    $export_data['user_id'] = $row['user_id'];
+    $export_data['username'] = $row['username'];
+    $export_data['fname'] = $row['fname'];
+    $export_data['lname'] = $row['lname'];
+    $export_data['email'] = $row['email'];
+    $export_data['secondary_email'] = $row['secondary_email'];
+    $export_data['function'] = $row['function'];
 
-            td {
-                font-weight: lighter;
-            }
-        </style>
-    </head>
+    $user_id = $row['user_id'];
 
-    <body>
-    <section>
-        <h1>HTML page</h1>
-        <!-- TABLE CONSTRUCTION -->
-        <table>
-            <tr>
-                <th>User ID</th>
-                <th>Username</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Secondary Email</th>
-                <th>Function</th>
-            </tr>
-            <!-- PHP CODE TO FETCH DATA FROM ROWS -->
-            <?php
-            // LOOP TILL END OF DATA
-            //select all the data of a user
-            $sql = "SELECT user_id,username,fname,lname,email,secondary_email,function FROM users where username = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $username);
-            //$export_data  = array();
-            $stmt->execute();
-            $result = $stmt->get_result();
+    $stmt = $conn->prepare("SELECT firstname ,lastname,relationship,visit_nature,source_of_income,visit_start,visit_end FROM appointments WHERE person_id = ?");
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-            while($rows=$result->fetch_assoc())
-            {
-                $user_id = $rows['user_id'];
-                ?>
-                <tr>
-                    <!-- FETCHING DATA FROM EACH
-                        ROW OF EVERY COLUMN -->
-                    <td><?php echo $rows['user_id'];?></td>
-                    <td><?php echo $rows['username'];?></td>
-                    <td><?php echo $rows['fname'];?></td>
-                    <td><?php echo $rows['lname'];?></td>
-                    <td><?php echo $rows['email'];?></td>
-                    <td><?php echo $rows['secondary_email'];?></td>
-                    <td><?php echo $rows['function'];?></td>
-                </tr>
-                <?php
-            }
+    $appointments = array();
+    while ($row = $result->fetch_assoc()) {
+        $appointments[] = $row;
+    }
+    //print_r($appointments);
 
 
-            ?>
-        </table>
-        <br><br>
-        <table>
-            <tr>
-                <th>Prisoner first name</th>
-                <th>Prisoner last name</th>
-                <th>Relationship</th>
-                <th>Nature of the visit</th>
-                <th>Income source</th>
-                <th>Visit start</th>
-                <th>Visit end</th>
-                <th>Prisoner sentence start date</th>
-                <th>Prisoner sentence end date</th>
-                <th>Prisoner sentence category</th>
-            </tr>
-            <!-- PHP CODE TO FETCH DATA FROM ROWS -->
-            <?php
-            // LOOP TILL END OF DATA
-            $sql2 = "SELECT * FROM appointments WHERE person_id = ?";
-            $stmt2 = $conn->prepare($sql2);
-            $stmt2->bind_param("i", $user_id);
-            $stmt2->execute();
-            $result2 = $stmt2->get_result();
-            $row2 = $result2->fetch_assoc();
-            print_r($row2);
-
-
-            //get the inmate for each appointment
-
-            while ($row2 != null) {
-                // $inmate_id = $row['inmate_id'];
-                $inmate_first_name = $row2['firstname'];
-                $inmate_last_name = $row2['lastname'];
-
-                $stmt3 = $conn->prepare("SELECT fname,lname, sentence_start_date,sentence_duration ,sentence_category FROM inmates WHERE fname = ? AND lname = ? ");
-                $stmt3->bind_param("ss", $inamte_first_name, $inmate_last_name);
-                $stmt3->execute();
-                $result3 = $stmt3->get_result();
-                $inmate = $result3->fetch_assoc();
-                //put in an array all the data of the inmates
-
-
-            }
-
-
-            ?>
-        </table>
-    </section>
-    </body>
-
-    </html>
-<?php
 
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>HTML Export</title>
+    <!-- CSS FOR STYLING THE PAGE -->
+    <style>
+        :root {
+            --darkblue: #32435f;
+            --lightblue: #85abef;
+            --offpink: #a67f78;
+            --capuccino: #8f8681;
+            --offwhite: #e1dcd9;
+            --lightcapuccino: #cccbc9;
+            --brown: #5c4b4b;
+        }
+
+        body {
+            background-color: var(--offwhite);
+            font-family: 'Roboto', sans-serif;
+        }
+
+
+        table {
+            margin: 0 auto;
+            font-size: large;
+            border: 1px solid var(--brown);
+        }
+
+        h1 {
+            text-align: center;
+            color: var(--darkblue);
+            font-size: xx-large;
+
+        }
+
+        td {
+            background-color: var(--offwhite);
+            border: 1px solid var(--brown);
+        }
+
+        th,
+        td {
+            font-weight: bold;
+            border: 1px solid var(--brown);
+            padding: 10px;
+            text-align: center;
+            color: var(--darkblue);
+        }
+
+        td {
+            font-weight: lighter;
+        }
+    </style>
+</head>
+
+<body>
+<section>
+    <h1>Detention Admin</h1>
+    <!-- TABLE CONSTRUCTION -->
+    <table>
+        <tr>
+            <th>User Id</th>
+            <th>Username</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Secondary Email</th>
+            <th>Function</th>
+        </tr>
+        <tr>
+            <td><?php echo $export_data['user_id']; ?></td>
+            <td><?php echo $export_data['username']; ?></td>
+            <td><?php echo $export_data['fname']; ?></td>
+            <td><?php echo $export_data['lname']; ?></td>
+            <td><?php echo $export_data['email']; ?></td>
+            <td><?php echo $export_data['secondary_email']; ?></td>
+            <td><?php echo $export_data['function']; ?></td>
+        </tr>
+
+    </table>
+    <br>
+    <table>
+        <tr>
+            <th>Prisoner first name</th>
+            <th>Prisoner last name</th>
+            <th>Relationship</th>
+            <th>Nature of the visit</th>
+            <th>Income source</th>
+            <th>Visit start</th>
+            <th>Visit end</th>
+            <th>Prisoner sentence start date</th>
+            <th>Prisoner sentence duration</th>
+            <th>Prisoner sentence category</th>
+        </tr>
+
+            <?php
+            $i = 0;
+            $n = count($appointments);
+                while( $i < $n){
+                    ?>
+        <tr>
+                    <td><?php echo $appointments[$i]['firstname']; ?></td>
+                    <td><?php echo $appointments[$i]['lastname']; ?></td>
+                    <td><?php echo $appointments[$i]['relationship']; ?></td>
+                    <td><?php echo $appointments[$i]['visit_nature']; ?></td>
+                    <td><?php echo $appointments[$i]['source_of_income']; ?></td>
+                    <td><?php echo $appointments[$i]['visit_start']; ?></td>
+                    <td><?php echo $appointments[$i]['visit_end']; ?></td>
+            <?php
+            $stmt = $conn->prepare("SELECT sentence_start_date, sentence_duration, sentence_category FROM inmates WHERE fname = ? AND lname = ?");
+            $stmt->bind_param("ss", $appointments[$i]['firstname'], $appointments[$i]['lastname']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_assoc();
+            ?>
+                    <td><?php echo $row['sentence_start_date']; ?></td>
+                    <td><?php echo $row['sentence_duration']; ?></td>
+                    <td><?php echo $row['sentence_category']; ?></td>
+        </tr>
+                    <?php
+                    $i = $i + 1;
+                }
+            ?>
+
+    </table>
+</section>
+</body>
+
+</html>
